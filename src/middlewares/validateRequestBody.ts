6ef3a0ab8f87005172312122c8e7b8af0ab4e6ed
Érico <oliv.ericorib@gmail.com>
2025-09-23 18:41:24 -0300

    fix: ajustar corpo de resposta de erros do zod

--- src/middlewares/validateRequestBody.ts
+++ src/middlewares/validateRequestBody.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodType, treeifyError } from "zod";
+import { ZodType, flattenError, treeifyError } from "zod";
 
 export default function validateRequestBody(schema: ZodType) {
   return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
-      const { errors } = treeifyError(result.error);
+      const { fieldErrors } = flattenError(result.error);
 
-      return res.status(400).json({ 
+      return res.status(422).json({ 
         message: 'Falha ao validar dados de requisição',
-        errors
+        errors: fieldErrors
       });
     }
 
--- src/schemas/create-user.schema.ts
+++ src/schemas/create-user.schema.ts
@@ -1,9 +1,9 @@
 import z from "zod";
 
 export const createUserSchema = z.object({
-  name: z.string().min(3).max(50),
-  email: z.string().min(10).max(100),
-  password: z.string().min(8).max(100),
+  name: z.string().min(3, 'Nome deve ter no mínimo 3 caracteres').max(50, 'Nome deve ter no máximo 50 caracteres'),
+  email: z.string().min(10, 'E-mail deve ter no mínimo 10 caracteres').max(100, 'E-mail deve ter no máximo 100 caracteres'),
+  password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres'),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
