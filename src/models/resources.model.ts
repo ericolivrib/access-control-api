5e0b9784f640a5c3cb8a97afeed00a924fa0aade
Érico <oliv.ericorib@gmail.com>
2025-09-24 21:35:50 -0300

    feat: adicionar campo `description` em resources.model.ts

--- src/models/resources.model.ts
+++ src/models/resources.model.ts
@@ -3,7 +3,8 @@ import { DataTypes, Model, Optional } from "sequelize";
 
 interface ResourceAttributes {
   id: number;
-  label: string
+  label: string;
+  description: string;
 }
 
 type ResourceCreationAttributes = Optional<ResourceAttributes, 'id'>;
@@ -20,6 +21,10 @@ Resource.init({
     type: DataTypes.STRING(20),
     allowNull: false,
     unique: true,
+  },
+  description: {
+    type: DataTypes.STRING(100),
+    allowNull: true,
   }
 }, {
   sequelize: datasource,
