cf2b4c873d400fd4a1ecd6076bc1f7297945de5c
Érico <oliv.ericorib@gmail.com>
2025-09-29 22:20:48 -0300

    feat: ajustar coerção de string para date

--- src/schemas/grant-access.schema.ts
+++ src/schemas/grant-access.schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 
 export const grantAccessSchema = z.object({
   permissionType: z.enum(PERMISSION_TYPES, 'Permissão não existente'),
-  expiresAt: z.date().min(new Date(), 'Data de expiração não pode estar no passado')
+  expiresAt: z.coerce.date().min(new Date(), 'Data de expiração não pode estar no passado')
 });
 
 export type GrantAccessSchema = z.infer<typeof grantAccessSchema>;
