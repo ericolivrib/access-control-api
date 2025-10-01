5cc20516d9485d1b6e00d4d8f780853766c1b711
Érico <oliv.ericorib@gmail.com>
2025-10-01 19:32:41 -0300

    feat: ajustar accessSchema

--- src/schemas/access.schema.ts
+++ src/schemas/access.schema.ts
@@ -5,11 +5,13 @@ import z from "zod";
 export const accessSchema = z.object({
   id: z.uuid(),
   userId: z.uuid(),
-  permissionType: z.enum(PERMISSION_TYPES),
+  permission: z.object({
+    type: z.enum(PERMISSION_TYPES)
+  }),
   status: z.enum(ACCESS_STATUSES),
-  expiresAt: z.date(),
+  expiresAt: z.date().nullable(),
   grantedAt: z.date(),
-  revokedAt: z.date()
+  revokedAt: z.date().nullable()
 });
 
 export type AccessSchema = z.infer<typeof accessSchema>;
